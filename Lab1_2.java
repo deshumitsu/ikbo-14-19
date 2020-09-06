fbe5aefc3d6a6ee2f5cd8f7d3d514927f2a52462
deshumitsu <70576193+deshumitsu@users.noreply.github.com>
2020-09-06 21:47:36 +0300

    Add files via upload

---
+++ Lab1_2.java
@@ -0,0 +1,12 @@
+package ru.mirea.lab1;
+
+import java.util.Scanner;
+
+public class Lab1_2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        for (int i = 0; i < 10; i++)
+            System.out.println(i);
+
+    }
+}
